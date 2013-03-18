4842479c63f958ef7549ef2c96f55a453a42ce4b
urazen66 <urazen@gmail.com>
2013-03-18 21:38:35 +0100

    message

--- Gemfile
+++ Gemfile
@@ -5,7 +5,16 @@ gem 'rails', '3.2.11'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+group :development, :test do
+	gem 'sqlite3'
+	gem 'rspec-rails', '2.11.0'
+end
+group :test do
+	gem 'capybara', '1.1.2'
+end
+group :production do
+	gem 'pg', '0.12.2'
+end
 
 
 # Gems used only for assets and not required
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TemplateApp::Application.config.secret_token = 'adda5d8d4e4aeb844cbd3a05c72b5795b9af61dfd39b99f58c694869ef7ae4f20dc07fadb837b9d28008d0863c79f7c8940248e6124fd8da8d93a47266b684cf'
+TemplateApp::Application.config.secret_token = '33292c82a19ca884f0c6019fd6cdf0c5816c49b140e36a78713adec1d8b0de11e487a1ef8898e21f51a90808731d1d4cc4673e75824d83ff8c72724d54436e65'
